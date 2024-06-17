0c6d9a8cb43ae8ddf7a3d4a4551078ada32142ea
Özlem KOCAHARZEN <ozlmmkocaharzen@gmail.com>
2024-06-17 18:05:17 +0300

    Chapter 14.1: Admin Layout with partial views.

--- Infrastructure/CarBook.Persistence/Context/CarBookContext.cs
+++ Infrastructure/CarBook.Persistence/Context/CarBookContext.cs
@@ -12,7 +12,7 @@ namespace CarBook.Persistence.Context
     {
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlServer("server= localhost; initial catalog = CarBookDb;integrated security = true; TrustServerCertificate=True");
+            optionsBuilder.UseSqlServer("server= .; initial catalog = CarBookDb;integrated security = true; TrustServerCertificate=True");
         }
         public DbSet<About> Abouts { get; set; }
         public DbSet<Banner> Banners { get; set; }
